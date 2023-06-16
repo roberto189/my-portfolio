a9d709a26465da66a83aa6db2b6987d89c292df6
roberto189 <126641489+roberto189@users.noreply.github.com>
2023-06-16 13:36:05 -0700

    yo

---
+++ src/components/footer/footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import { Link } from "react-router-dom"
+
+
+ function footer() {
+  return (
+    <footer>
+        <ul>
+        footer
+        </ul>
+        
+    </footer>
+  );
+}
+export default footer;
